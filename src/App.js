a8486997e2753a9153ec2eb2db8ba1aee5468756
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-12-13 04:33:12 -0800

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
-import RightBar from "./components/RightBar";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
-import { Box, Stack } from "@mui/material";
 import Add from "./components/Add";
+import { useState } from "react";
+import Rightbar from "./components/RightBar";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Navbar />
-      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
-        <Sidebar />
-        <Feed />
-        <RightBar />
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
